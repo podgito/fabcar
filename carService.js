01b229ad5bd310c9abbcd7cde5fe7bda3473aae2
Padraic Duffy <Padraic.Duffy@version1.com>
2017-12-20 10:07:56 +0000

    Added carss api

--- carService.js
+++ carService.js
@@ -1,10 +1,10 @@
 const express = require('express')
-var Fabric_Client = require('fabric-client');
+// var Fabric_Client = require('fabric-client');
 var path = require('path');
 var util = require('util');
 var os = require('os');
 
-var fabric_client = new Fabric_Client();
+// var fabric_client = new Fabric_Client();
 
 const app = express()
 
@@ -12,8 +12,8 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/padraic', (req, res) => res.send('Hello Padraic!'))
 
 
-app.get('cars', (req, res) => {
-    
+app.get('/cars', (req, res) => {
+    res.send(' [{"Key":"CAR0", "Record":{"colour":"blue","make":"Toyota","model":"Prius","owner":"Tomoko"}},{"Key":"CAR1", "Record":{"colour":"red","make":"Ford","model":"Mustang","owner":"Brad"}},{"Key":"CAR2", "Record":{"colour":"green","make":"Hyundai","model":"Tucson","owner":"Jin Soo"}},{"Key":"CAR3", "Record":{"colour":"yellow","make":"Volkswagen","model":"Passat","owner":"Max"}},{"Key":"CAR4", "Record":{"colour":"black","make":"Tesla","model":"S","owner":"Adriana"}},{"Key":"CAR5", "Record":{"colour":"purple","make":"Peugeot","model":"205","owner":"Michel"}},{"Key":"CAR6", "Record":{"colour":"white","make":"Chery","model":"S22L","owner":"Aarav"}},{"Key":"CAR7", "Record":{"colour":"violet","make":"Fiat","model":"Punto","owner":"Pari"}},{"Key":"CAR8", "Record":{"colour":"indigo","make":"Tata","model":"Nano","owner":"Valeria"}},{"Key":"CAR9", "Record":{"colour":"brown","make":"Holden","model":"Barina","owner":"Shotaro"}}]');
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
