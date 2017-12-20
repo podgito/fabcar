9259ab42d40f29c75512477b4ae87b61a2496e2e
Farron Flaherty <farron.flaherty@version1.com>
2017-12-20 18:26:21 +0000

    resetting username

--- registerUser.js
+++ registerUser.js
@@ -53,16 +53,16 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
     // at this point we should have the admin user
     // first need to register the user with the CA server
-    return fabric_ca_client.register({enrollmentID: 'fabcarUser', affiliation: 'org1.department1'}, admin_user);
+    return fabric_ca_client.register({enrollmentID: 'user1', affiliation: 'org1.department1'}, admin_user);
 }).then((secret) => {
     // next we need to enroll the user with CA server
-    console.log('Successfully registered fabcarUser - secret:'+ secret);
+    console.log('Successfully registered user1 - secret:'+ secret);
 
-    return fabric_ca_client.enroll({enrollmentID: 'fabcarUser', enrollmentSecret: secret});
+    return fabric_ca_client.enroll({enrollmentID: 'user1', enrollmentSecret: secret});
 }).then((enrollment) => {
-  console.log('Successfully enrolled member user "fabcarUser" ');
+  console.log('Successfully enrolled member user "user1" ');
   return fabric_client.createUser(
-     {username: 'fabcarUser',
+     {username: 'user1',
      mspid: 'Org1MSP',
      cryptoContent: { privateKeyPEM: enrollment.key.toBytes(), signedCertPEM: enrollment.certificate }
      });
@@ -71,7 +71,7 @@ Fabric_Client.newDefaultKeyValueStore({ path: store_path
 
      return fabric_client.setUserContext(member_user);
 }).then(()=>{
-     console.log('fabcarUser was successfully registered and enrolled and is ready to intreact with the fabric network');
+     console.log('user1 was successfully registered and enrolled and is ready to intreact with the fabric network');
 
 }).catch((err) => {
     console.error('Failed to register: ' + err);
