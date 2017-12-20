cec92e75c00ebe529d7d317d281e7405b020e7b6
Padraic Duffy <Padraic.Duffy@version1.com>
2017-12-20 10:15:10 +0000

    Added CORS

--- carService.js
+++ carService.js
@@ -8,6 +8,12 @@ var os = require('os');
 
 const app = express()
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/padraic', (req, res) => res.send('Hello Padraic!'))
 
