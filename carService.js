6b9cceaeff15648838c474758351e9299d0914ab
Farron Flaherty <farron.flaherty@version1.com>
2017-12-21 17:06:19 +0000

    set header

--- carService.js
+++ carService.js
@@ -18,12 +18,13 @@ var store_path = '/home/blockchain/projects/fabric-samples/fabcar/hfc-key-store'
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Content-Type', 'application/json');
     next();
 });
 
