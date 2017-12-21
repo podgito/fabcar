72978547078d0f292aa2011c7dff23f9fb962ca6
Farron Flaherty <farron.flaherty@version1.com>
2017-12-21 17:49:56 +0000

    foobar

--- fabcar/src/app/services/add-car.service.ts
+++ fabcar/src/app/services/add-car.service.ts
@@ -23,6 +23,6 @@ export class AddCarService {
     const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
 
     console.log(newCar);
-    return this.http.post('http://localhost:3000/addCar/', newCar, {headers : headers});
+    return this.http.post('http://localhost:3000/addCar/', {'foo':'bar'}, {headers : headers});
   }
 }
