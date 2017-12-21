a1138a49b22984a4b2e192b3a857ebc5cd7e6e87
Farron Flaherty <farron.flaherty@version1.com>
2017-12-21 10:22:12 +0000

    logging

--- carService.js
+++ carService.js
@@ -20,8 +20,9 @@ app.get('/padraic', (req, res) => res.send('Hello Padraic!'))
 
 app.get('/cars', (req, res) => {
     // setup the fabric network
-    res.send(queryFabric('queryAllCars', []));
-                
+    var test = queryFabric('queryAllCars', []);
+    console.log("Returned is ", test);            
+
     //res.send('[{"Key":"CAR0", "Record":{"colour":"blue","make":"Toyota","model":"Prius","owner":"Tomoko"}},{"Key":"CAR1", "Record":{"colour":"red","make":"Ford","model":"Mustang","owner":"Brad"}},{"Key":"CAR2", "Record":{"colour":"green","make":"Hyundai","model":"Tucson","owner":"Jin Soo"}},{"Key":"CAR3", "Record":{"colour":"yellow","make":"Volkswagen","model":"Passat","owner":"Max"}},{"Key":"CAR4", "Record":{"colour":"black","make":"Tesla","model":"S","owner":"Adriana"}},{"Key":"CAR5", "Record":{"colour":"purple","make":"Peugeot","model":"205","owner":"Michel"}},{"Key":"CAR6", "Record":{"colour":"white","make":"Chery","model":"S22L","owner":"Aarav"}},{"Key":"CAR7", "Record":{"colour":"violet","make":"Fiat","model":"Punto","owner":"Pari"}},{"Key":"CAR8", "Record":{"colour":"indigo","make":"Tata","model":"Nano","owner":"Valeria"}},{"Key":"CAR9", "Record":{"colour":"brown","make":"Holden","model":"Barina","owner":"Shotaro"}}]');
 })
 
