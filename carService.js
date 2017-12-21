eb0df72b95001fc4e2bf621518f78a253c5efeb8
Farron Flaherty <farron.flaherty@version1.com>
2017-12-21 14:42:30 +0000

    removing trailing slash

--- carService.js
+++ carService.js
@@ -8,6 +8,8 @@ var fabric_client = new Fabric_Client();
 var channel = fabric_client.newChannel('mychannel');
 var peer = fabric_client.newPeer('grpc://localhost:7051');
 channel.addPeer(peer);
+var order = fabric_client.newOrderer('grpc://localhost:7050')
+channel.addOrderer(order);
 
 const app = express();
 
@@ -23,7 +25,7 @@ app.get('/padraic', (req, res) => res.send('Hello Padraic!'))
 
 
 app.get('/cars', (req, res) => {
-    queryFabric('queryAllCars', [], res)       
+    queryFabric('queryAllCars', [], res)
 
     //res.send('[{"Key":"CAR0", "Record":{"colour":"blue","make":"Toyota","model":"Prius","owner":"Tomoko"}},{"Key":"CAR1", "Record":{"colour":"red","make":"Ford","model":"Mustang","owner":"Brad"}},{"Key":"CAR2", "Record":{"colour":"green","make":"Hyundai","model":"Tucson","owner":"Jin Soo"}},{"Key":"CAR3", "Record":{"colour":"yellow","make":"Volkswagen","model":"Passat","owner":"Max"}},{"Key":"CAR4", "Record":{"colour":"black","make":"Tesla","model":"S","owner":"Adriana"}},{"Key":"CAR5", "Record":{"colour":"purple","make":"Peugeot","model":"205","owner":"Michel"}},{"Key":"CAR6", "Record":{"colour":"white","make":"Chery","model":"S22L","owner":"Aarav"}},{"Key":"CAR7", "Record":{"colour":"violet","make":"Fiat","model":"Punto","owner":"Pari"}},{"Key":"CAR8", "Record":{"colour":"indigo","make":"Tata","model":"Nano","owner":"Valeria"}},{"Key":"CAR9", "Record":{"colour":"brown","make":"Holden","model":"Barina","owner":"Shotaro"}}]');
 })
@@ -33,12 +35,17 @@ app.get('/carHistory/:key', (req, res) => {
     //res.send('[{"TxId":"30e19db83970864f8b28ffd9edc6fd0d89efb14b06563eae12af8890de6445f4", "Value":{"make":"Lambo","model":"Guido","colour":"Yellow","owner":"Alex"}, "Timestamp":"2017-12-19 13:48:28.891 +0000 UTC", "IsDelete":"false"},{"TxId":"fee488c362c4eaf3f27214ead8946d5f95e47733b4df68c4b7468ef822a8f59d", "Value":{"make":"Lambo","model":"Guido","colour":"Yellow","owner":"Padraic"}, "Timestamp":"2017-12-19 13:49:02.21 +0000 UTC", "IsDelete":"false"},{"TxId":"3576a5fbaba590cf348b9e3cf2b985f8595e8279c57f3e469c9edecdbc8417a3", "Value":{"make":"Lambo","model":"Guido","colour":"Yellow","owner":"Farron"}, "Timestamp":"2017-12-19 13:49:19.402 +0000 UTC", "IsDelete":"false"}]');
 })
 
+app.post('/addCar', (req, res) => {
+    console.log("Add Car");
+    invokeFabric('createCar', ['CAR10', 'Lamborghini', 'Gallardo', 'yellow', 'Faz'])
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-function queryFabric(fcn, args, res){
+function queryFabric(fcn, args, res) {
 
     var member_user = null;
-    var store_path = '/home/blockchain/projects/fabric-samples/fabcar/hfc-key-store'// path.join('home/blockchain/projects/fabric-samples/hfc-key-store');
+    var store_path = '/home/blockchain/projects/fabric-samples/fabcar/hfc-key-store';// path.join('home/blockchain/projects/fabric-samples/hfc-key-store');
     console.log('Store path:' + store_path);
     var tx_id = null;
 
@@ -92,4 +99,139 @@ function queryFabric(fcn, args, res){
     }).catch((err) => {
         console.error('Failed to query successfully :: ' + err);
     });
+}
+
+function invokeFabric(fcn, args) {
+    Fabric_Client.newDefaultKeyValueStore({
+        path: store_path
+    }).then((state_store) => {
+        // assign the store to the fabric client
+        fabric_client.setStateStore(state_store);
+        var crypto_suite = Fabric_Client.newCryptoSuite();
+        // use the same location for the state store (where the users' certificate are kept)
+        // and the crypto store (where the users' keys are kept)
+        var crypto_store = Fabric_Client.newCryptoKeyStore({ path: store_path });
+        crypto_suite.setCryptoKeyStore(crypto_store);
+        fabric_client.setCryptoSuite(crypto_suite);
+
+        // get the enrolled user from persistence, this user will sign all requests
+        return fabric_client.getUserContext('user1', true);
+    }).then((user_from_store) => {
+        if (user_from_store && user_from_store.isEnrolled()) {
+            console.log('Successfully loaded user1 from persistence');
+            member_user = user_from_store;
+        } else {
+            throw new Error('Failed to get user1.... run registerUser.js');
+        }
+
+        // get a transaction id object based on the current user assigned to fabric client
+        tx_id = fabric_client.newTransactionID();
+        console.log("Assigning transaction_id: ", tx_id._transaction_id);
+
+        // createCar chaincode function - requires 5 args, ex: args: ['CAR12', 'Honda', 'Accord', 'Black', 'Tom'],
+        // changeCarOwner chaincode function - requires 2 args , ex: args: ['CAR10', 'Barry'],
+        // must send the proposal to endorsing peers
+        var request = {
+            //targets: let default to the peer assigned to the client
+            chaincodeId: 'fabcar',
+            fcn: fcn,
+            args: args,
+            chainId: 'mychannel',
+            txId: tx_id
+        };
+
+        // send the transaction proposal to the peers
+        return channel.sendTransactionProposal(request);
+    }).then((results) => {
+        var proposalResponses = results[0];
+        var proposal = results[1];
+        let isProposalGood = false;
+        if (proposalResponses && proposalResponses[0].response &&
+            proposalResponses[0].response.status === 200) {
+            isProposalGood = true;
+            console.log('Transaction proposal was good');
+        } else {
+            console.error('Transaction proposal was bad');
+        }
+        if (isProposalGood) {
+            console.log(util.format(
+                'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s"',
+                proposalResponses[0].response.status, proposalResponses[0].response.message));
+
+            // build up the request for the orderer to have the transaction committed
+            var request = {
+                proposalResponses: proposalResponses,
+                proposal: proposal
+            };
+
+            // set the transaction listener and set a timeout of 30 sec
+            // if the transaction did not get committed within the timeout period,
+            // report a TIMEOUT status
+            var transaction_id_string = tx_id.getTransactionID(); //Get the transaction ID string to be used by the event processing
+            var promises = [];
+
+            var sendPromise = channel.sendTransaction(request);
+            promises.push(sendPromise); //we want the send transaction first, so that we know where to check status
+
+            // get an eventhub once the fabric client has a user assigned. The user
+            // is required bacause the event registration must be signed
+            let event_hub = fabric_client.newEventHub();
+            event_hub.setPeerAddr('grpc://localhost:7053');
+
+            // using resolve the promise so that result status may be processed
+            // under the then clause rather than having the catch clause process
+            // the status
+            let txPromise = new Promise((resolve, reject) => {
+                let handle = setTimeout(() => {
+                    event_hub.disconnect();
+                    resolve({ event_status: 'TIMEOUT' }); //we could use reject(new Error('Trnasaction did not complete within 30 seconds'));
+                }, 3000);
+                event_hub.connect();
+                event_hub.registerTxEvent(transaction_id_string, (tx, code) => {
+                    // this is the callback for transaction event status
+                    // first some clean up of event listener
+                    clearTimeout(handle);
+                    event_hub.unregisterTxEvent(transaction_id_string);
+                    event_hub.disconnect();
+
+                    // now let the application know what happened
+                    var return_status = { event_status: code, tx_id: transaction_id_string };
+                    if (code !== 'VALID') {
+                        console.error('The transaction was invalid, code = ' + code);
+                        resolve(return_status); // we could use reject(new Error('Problem with the tranaction, event status ::'+code));
+                    } else {
+                        console.log('The transaction has been committed on peer ' + event_hub._ep._endpoint.addr);
+                        resolve(return_status);
+                    }
+                }, (err) => {
+                    //this is the callback if something goes wrong with the event registration or processing
+                    reject(new Error('There was a problem with the eventhub ::' + err));
+                });
+            });
+            promises.push(txPromise);
+
+            return Promise.all(promises);
+        } else {
+            console.error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+            throw new Error('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
+        }
+    }).then((results) => {
+        console.log('Send transaction promise and event listener promise have completed');
+        // check the results in the order the promises were added to the promise all list
+        if (results && results[0] && results[0].status === 'SUCCESS') {
+            console.log('Successfully sent transaction to the orderer.');
+        } else {
+            console.error('Failed to order the transaction. Error code: ' + response.status);
+        }
+
+        if (results && results[1] && results[1].event_status === 'VALID') {
+            console.log('Successfully committed the change to the ledger by the peer');
+        } else {
+            console.log('Transaction failed to be committed to the ledger due to ::' + results[1].event_status);
+        }
+    }).catch((err) => {
+        console.error('Failed to invoke successfully :: ' + err);
+    });
+
+
 }
